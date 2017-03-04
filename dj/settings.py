bde1496532b1624dcb8be960b870ca5f6eede7e2
sharath <sharath.savasere@gmail.com>
2017-03-04 11:53:11 +0530

    new commit

--- dj/settings.py
+++ dj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+r'\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Question,Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
